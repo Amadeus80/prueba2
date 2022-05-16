eb45ae8ebe3ca88a8b3e36777fb85f6857723d1d
Pablo Acosta <acostaortizpablo@gmail.com>
2022-05-16 10:30:59 +0200

    Creada clase calculadora y main

---
+++ src/calculadorabash/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorabash;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    calculadora Casio1 = new calculadora ( 50, 15, "Casio" , "CRD-230"); 
+       
+       Casio1.suma();
+       Casio1.resta();
+       Casio1.multiplicacion();
+       Casio1.division();
+       
+       Casio1.setNum1(25);
+       Casio1.setNum2(80);
+       Casio1.suma();
+       System.out.println("Los valores de la calculadora son "+ Casio1.getNum1() + " y " + Casio1.getNum2());
+       
+       calculadora Casio2 = new calculadora ( "Casio" , "CTT-40");
+       calculadora Casio3 = new calculadora ( );
+       
+       System.out.println("La marca y modelo de la calculadora Casio2 son "+ Casio2.getMarca() + " y " + Casio2.getModelo());
+       Casio2.setNum1(10);
+       Casio2.setNum2(2);
+       Casio2.suma();
+    }    }
+    
+
